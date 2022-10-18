afffaf3add90558bd42508824725fed7747b196b
Aline Ferreira <aline.fn80@gmail.com>
2022-10-18 16:50:40 -0300

    primeira activity no android Studio

---
+++ app/src/main/java/com/example/atividade_06/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.atividade_06;
+
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.atividade_06.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Activity_02.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
